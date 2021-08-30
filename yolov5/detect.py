3f56588e74227a169f8046ed45868c2e75926a94
yang-junhwan <87357037+yang-junhwan@users.noreply.github.com>
2021-08-30 17:17:16 +0900

    detect.py_1.3 끊어지는 영상 연결

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -23,6 +23,7 @@ from utils.general import check_img_size, check_requirements, check_imshow, colo
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_sync
 
+import pandas as pd
 
 @torch.no_grad()
 def run(weights='yolov5s.pt',  # model.pt path(s)
@@ -63,6 +64,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
+    #비디오 만들기 제어
     mkvid=True
 
     # Directories
@@ -243,20 +245,29 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
 
     print(f'Done. ({time.time() - t0:.3f}s)')
 
-    folder_count=0
-    #연속된 프레임 리스트에 있는 것만 검사해주는 반복문 by 양
-    for frame_list in frame_all_arr:
-        #비디오 저장용 초기화
-        vid_path, vid_writer = [None] * bs, [None] * bs
+    #끊기는거 확인
+    frame_all_check=[]
+
+    #끊어져있는 부분 연결
+    for i in range(1,len(frame_all_arr)):
+        if frame_all_arr[i][0]-frame_all_arr[i-1][1]<60:
+            cut_check=True
+            frame_all_check.append([frame_all_arr[i-1][0],frame_all_arr[i][1],cut_check])
+        else:
+            cut_check=False
+            frame_all_check.append([frame_all_arr[i][0],frame_all_arr[i][1],cut_check])
+
+    #frame_all_arr가 한줄일경우
+    if len(frame_all_arr)==1:
+        frame_all_check.append([frame_all_arr[0][0],frame_all_arr[0][1],False])
+    
+    frame_start_end=[]
+    #구간 확정 by 양
+    for frame_list in frame_all_check:
         #원하는 곳만 이미지를 읽어오는 클래스
         dataset = LoadImages_Select(source, img_size=imgsz, stride=stride, frame_arr=frame_list)
-        #연속된 프레임별 이미지가 저장될 폴더 생성  
-        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/images/%s/%s" %(folder_name,folder_count))
-        folder_path.mkdir(parents=True, exist_ok=True)
-        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/labels/%s/%s" %(folder_name,folder_count))
-        folder_path.mkdir(parents=True, exist_ok=True)
-        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/videos/%s/%s" %(folder_name,folder_count))
-        folder_path.mkdir(parents=True, exist_ok=True)
+        start_=0
+        end_=0
         for path, img, im0s, vid_cap in dataset:          
             if pt:
                 img = torch.from_numpy(img).to(device)
@@ -320,40 +331,139 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                                 save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
                 #by 양
                 if 'zoom_in' in s:
-                    #박스치기
-                    '''
+                    if start_==0:
+                        start_=frame
+                    end_=frame
+
+                # Print time (inference + NMS)
+                print(f'{s}Done. ({t2 - t1:.3f}s)')
+                #print(frame_all_arr)
+                # Stream results
+                if view_img:
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(1)  # 1 millisecond
+
+        if save_txt or save_img:
+            s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+            print(f"Results saved to {colorstr('bold', save_dir)}{s}")
+
+        if update:
+            strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
+
+        print(f'Done. ({time.time() - t0:.3f}s)')
+        frame_start_end.append([start_,end_,frame_list[2]])
+        start_=0
+
+    df=pd.DataFrame(frame_start_end)
+    df.columns=["start","end","cut_check"]
+
+    df.to_csv('/content/drive/MyDrive/dp/joomin/dataset/start_end/%s.csv' %(folder_name), sep=',', na_rep='NaN')
+
+    folder_count=0
+    #연속된 프레임 리스트에 있는 것만 검사해주는 반복문 by 양
+    for frame_list in frame_start_end:
+        #비디오 저장용 초기화
+        vid_path, vid_writer = [None] * bs, [None] * bs
+        #원하는 곳만 이미지를 읽어오는 클래스
+        dataset = LoadImages_Select(source, img_size=imgsz, stride=stride, frame_arr=frame_list)
+        #연속된 프레임별 이미지가 저장될 폴더 생성  
+        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/images/%s/%s" %(folder_name,folder_count))
+        folder_path.mkdir(parents=True, exist_ok=True)
+        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/labels/%s/%s" %(folder_name,folder_count))
+        folder_path.mkdir(parents=True, exist_ok=True)
+        folder_path = Path("/content/drive/MyDrive/dp/joomin/dataset/videos/%s/%s" %(folder_name,folder_count))
+        folder_path.mkdir(parents=True, exist_ok=True)
+
+        for path, img, im0s, vid_cap in dataset:          
+            if pt:
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+            elif onnx:
+                img = img.astype('float32')
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if len(img.shape) == 3:
+                img = img[None]  # expand for batch dim
+
+            # Inference
+            t1 = time_sync()
+            if pt:
+                visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+                pred = model(img, augment=augment, visualize=visualize)[0]
+            elif onnx:
+                pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
+
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            t2 = time_sync()
+
+            # Second-stage classifier (optional)
+            if classify:
+                pred = apply_classifier(pred, modelc, img, im0s)
+
+            # Process predictions
+            for i, det in enumerate(pred):  # detections per image
+                if webcam:  # batch_size >= 1
+                    p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
+                else:
+                    p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+                p = Path(p)  # to Path
+                save_path = str(save_dir / p.name)  # img.jpg
+                txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                s += '%gx%g ' % img.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
                     for *xyxy, conf, cls in reversed(det):
-                        c = int(cls)
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)'''
-                    #jpg파일 변환
-                    result, encoded_img = cv2.imencode(".jpg", im0)
-                    #이미지파일 저장
-                    encoded_img.tofile('/content/drive/MyDrive/dp/joomin/dataset/images/%s/%s/%s_%s.jpg' %(folder_name,folder_count,frame,conf))
-                    #라벨 정보 받아오기
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                    #라벨 저장
-                    with open('/content/drive/MyDrive/dp/joomin/dataset/labels/%s/%s/%s_%s.txt' %(folder_name,folder_count,frame,conf), 'a') as f:
-                        f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                    # Save results (image with detections)
-                    if mkvid:
-                        if dataset.mode == 'image':
-                            cv2.imwrite(save_path, im0)
-                        else:  # 'video' or 'stream'
-                            if vid_path[i] != save_path:  # new video
-                                vid_path[i] = save_path
-                                if isinstance(vid_writer[i], cv2.VideoWriter):
-                                    vid_writer[i].release()  # release previous video writer
-                                if vid_cap:  # video
-                                    fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                                    w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                                    h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                                else:  # stream
-                                    fps, w, h = 30, im0.shape[1], im0.shape[0]
-                                    save_path += '.mp4'
-                                vid_writer[i] = cv2.VideoWriter('/content/drive/MyDrive/dp/joomin/dataset/videos/%s/%s/%s_%s.mp4' %(folder_name,folder_count,folder_name,folder_count), cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                            vid_writer[i].write(im0)
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                            with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img or save_crop or view_img:  # Add bbox to image
+                            c = int(cls)  # integer class
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                            plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
+                            if save_crop:
+                                save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                #by 양
+                #jpg파일 변환
+                result, encoded_img = cv2.imencode(".jpg", im0)
+                #이미지파일 저장
+                encoded_img.tofile('/content/drive/MyDrive/dp/joomin/dataset/images/%s/%s/%s_%s.jpg' %(folder_name,folder_count,frame,conf))
+                #라벨 정보 받아오기
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                #라벨 저장
+                with open('/content/drive/MyDrive/dp/joomin/dataset/labels/%s/%s/%s_%s.txt' %(folder_name,folder_count,frame,conf), 'a') as f:
+                    f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                # Save results (image with detections)
+                if mkvid:
+                    if dataset.mode == 'image':
+                        cv2.imwrite(save_path, im0)
+                    else:  # 'video' or 'stream'
+                        if vid_path[i] != save_path:  # new video
+                            vid_path[i] = save_path
+                            if isinstance(vid_writer[i], cv2.VideoWriter):
+                                vid_writer[i].release()  # release previous video writer
+                            if vid_cap:  # video
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            else:  # stream
+                                fps, w, h = 30, im0.shape[1], im0.shape[0]
+                                save_path += '.mp4'
+                            vid_writer[i] = cv2.VideoWriter('/content/drive/MyDrive/dp/joomin/dataset/videos/%s/%s/%s_%s.mp4' %(folder_name,folder_count,folder_name,folder_count), cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                        vid_writer[i].write(im0)
 
                 # Print time (inference + NMS)
                 print(f'{s}Done. ({t2 - t1:.3f}s)')
